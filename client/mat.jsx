089f02cc4654518fb3d0f89d8a8a6ce13ed28336
David Fudge <rkstar@mac.com>
2015-09-29 13:05:26 -0400

    using material-ui 0.12.1, meteor 1.2.0.2, meteor/react-packages, react-tap-event-plugin 0.1.7

---
+++ client/mat.jsx
@@ -0,0 +1,28 @@
+let {RaisedButton} = mui,
+  App = React.createClass({
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+  getChildContext(){
+    return {
+      muiTheme: mui.Styles.ThemeManager.getMuiTheme(mui.Styles.LightRawTheme)
+    }
+  },
+
+  _handleTap(e){
+    e.preventDefault()
+    console.log('it worked ------- !')
+    console.log(e)
+  },
+
+  render(){
+    return (
+      <RaisedButton label='tap me' onTouchTap={this._handleTap} />
+    )
+  }
+})
+
+Meteor.startup(()=>{
+  React.render(<App />, document.body)
+})
